5f91d0404542a14fba41ca5138715318d89af52a
Heorhiy <zaynyatyi@gmail.com>
2013-10-28 22:51:57 +0200

    Removed local debugger stillDebugging has default value now to prevent multythread error

--- mmg/Base.hx
+++ mmg/Base.hx
@@ -11,142 +11,150 @@ import haxe.CallStack;
  */
 class Base
 {
-	var threadStopped:Bool;
-	var stillDebugging:Bool;
-	var inputThread:Thread;
-	var debugQueue:Deque<Dynamic>;
-	var files:Array<String>;
-	var frame:Int;
-	var stack:Array<StackItem>;
-	var vars:Array<String>;
-	var dbg:Dynamic;
-	var arrayLimit:Int;
-	
-	public function new(startStopped:Bool)
-	{
-		frame = -1;
-		stillDebugging = init();
-		arrayLimit = 100;
-		dbg = {};
-		if (stillDebugging)
-		{
-			files = Debugger.getFiles();
-			threadStopped = false;
-			Debugger.setThread();
-			Debugger.setHandler(onDebug);
-			debugQueue= new Deque<Dynamic>();
-			inputThread = Thread.create(inputLoop);
-			if (startStopped)
-				Debugger.breakBad();
-		}
-	}
-	
-	// init just for override
-	function init() : Bool
-	{
-		return false;
-	}
-	
-	function onDebug()
-	{
-		threadStopped = true;
-		onStopped();
-		while(threadStopped && stillDebugging)
-		{
-			var job = debugQueue.pop(true);
-			job();
-		}
-	}
+    var threadStopped:Bool;
+    var stillDebugging:Bool = true;
+    var inputThread:Thread;
+    var debugQueue:Deque<Dynamic>;
+    var files:Array<String>;
+    var frame:Int;
+    var stack:Array<StackItem>;
+    var vars:Array<String>;
+    var dbg:Dynamic;
+    var arrayLimit:Int;
 
-	function onStopped() { }
-	
-	function onRunning() { }
-	
-	function onCloseInput() { }
-	
-	function onResult(inResult:String) { }
-	
-	function getNextCommand() : String { return "bye"; }
-	
-	function getStack()
-	{
-		stack = haxe.CallStack.callStack();
-		setFrame(1);
-	}
-	
-	function checkStack()
-	{
-		if (threadStopped && stack==null)
-		{
-			debugQueue.add( getStack );
-			waitDebugger(false);
-		}
-	}
+    public function new(startStopped:Bool)
+    {
+        frame = -1;
+        stillDebugging = init();
+        arrayLimit = 100;
+        dbg = {};
+        if (stillDebugging)
+        {
+            files = Debugger.getFiles();
+            threadStopped = false;
+            Debugger.setThread();
+            Debugger.setHandler(onDebug);
+            debugQueue= new Deque<Dynamic>();
+            inputThread = Thread.create(inputLoop);
+            if (startStopped)
+                Debugger.breakBad();
+        }
+    }
 
-	function setFrame(inFrame:Int)
-	{
-		if (stack!=null && inFrame>0 && inFrame <= stack.length )
-		{
-			frame = inFrame;
-			vars = Debugger.getStackVars(frame);
-		}
-	}
-	
-	function run()
-	{
-		stack = null;
-		vars = null;
-		debugQueue.add( function() { threadStopped = false; inputThread.sendMessage("running"); }  );
-		var result = Thread.readMessage(true);
-		onRunning();
-		onResult("ok");
-	}
-	
-	function waitDebugger(inSendResult:Bool=true)
-	{
-		debugQueue.add( function() inputThread.sendMessage("ok")  );
-		var result = Thread.readMessage(true);
-		if (inSendResult)
-		{
-			if (result!="ok")
-				onResult("Debugger out of sync");
-			else
-				onResult("ok");
-		}
-	}
-	
-	function inputLoop()
-	{
-		while (stillDebugging)
-		{
-			checkStack();
-			var command = getNextCommand();
-			var words = command.split(" ");
-			switch(words[0])
-			{
-				case "":
-				   onResult("");
-				   // Do nothing
+    // init just for override
+    function init() : Bool
+    {
+        return false;
+    }
 
-				case "bye":
-				   stillDebugging = false;
-				   debugQueue.add( function() { trace("bye"); }  );
-				   onResult("bye");
-				   run();
+    function onDebug()
+    {
+        threadStopped = true;
+        onStopped();
+        while(threadStopped && stillDebugging)
+        {
+            var job = debugQueue.pop(true);
+            job();
+        }
+    }
 
-				case "exit","quit":
-				   onResult("ok");
-				   Debugger.exit();
-				
-				case "cont","c":
-					if (!threadStopped)
-						onResult("Already running.");
-					else
-						run();
-				case _:
-					onResult("Unknown command:" + command);
-			}
-		}
-		onCloseInput();
-	}
+    function onStopped() { }
+
+    function onRunning() { }
+
+    function onCloseInput() { }
+
+    function onResult(inResult:String) { }
+
+    function showFiles() { }
+
+    function getNextCommand() : String { return "bye"; }
+
+    function getStack()
+    {
+        stack = haxe.CallStack.callStack();
+        setFrame(1);
+    }
+
+    function checkStack()
+    {
+        if (threadStopped && stack==null)
+        {
+            debugQueue.add( getStack );
+            waitDebugger(false);
+        }
+    }
+
+    function setFrame(inFrame:Int)
+    {
+        if (stack!=null && inFrame>0 && inFrame <= stack.length )
+        {
+            frame = inFrame;
+            vars = Debugger.getStackVars(frame);
+        }
+    }
+
+    function run()
+    {
+        stack = null;
+        vars = null;
+        debugQueue.add( function() { threadStopped = false; inputThread.sendMessage("running"); }  );
+        var result = Thread.readMessage(true);
+        onRunning();
+        onResult("ok");
+    }
+
+    function waitDebugger(inSendResult:Bool=true)
+    {
+        debugQueue.add( function() inputThread.sendMessage("ok")  );
+        var result = Thread.readMessage(true);
+        if (inSendResult)
+        {
+            if (result!="ok")
+                onResult("Debugger out of sync");
+            else
+                onResult("ok");
+        }
+    }
+
+    function inputLoop()
+    {
+        while (stillDebugging)
+        {
+            checkStack();
+            var command = getNextCommand();
+            var words = command.split(" ");
+            switch(words[0])
+            {
+                case "":
+                    onResult("");
+                    // Do nothing
+
+                case "bye":
+                    trace("bye in input loop");
+                    stillDebugging = false;
+                    debugQueue.add( function() { trace("bye"); }  );
+                    onResult("bye");
+                    run();
+
+                case "exit","quit":
+                    onResult("ok");
+                    Debugger.exit();
+
+                case "cont","c":
+                    if (!threadStopped)
+                        onResult("Already running.");
+                    else
+                        run();
+
+                case "files","fi":
+                  showFiles();
+                  onResult("ok");
+
+                case _:
+                    onResult("Unknown command:" + command);
+            }
+        }
+        onCloseInput();
+    }
 }
--- mmg/DebugServer.hx
+++ mmg/DebugServer.hx
@@ -15,137 +15,137 @@ import haxe.Timer;
 
 class DebugServer
 {
-   var dbg:Socket;
-   var server:Socket;
-   var readBuffer:haxe.io.Bytes;
-   var fromApp:haxe.io.Input;
-   var toApp:haxe.io.Output;
-   var port:Int;
-   var mainThread:Thread;
-   var readThread:Thread;
-   var going:Bool;
+    var dbg:Socket;
+    var server:Socket;
+    var readBuffer:haxe.io.Bytes;
+    var fromApp:haxe.io.Input;
+    var toApp:haxe.io.Output;
+    var port:Int;
+    var mainThread:Thread;
+    var readThread:Thread;
+    var going:Bool;
 
-   static var result = 0;
-   static var status = 1;
-   static var output = 2;
+    static var result = 0;
+    static var status = 1;
+    static var output = 2;
 
-  function new(inHost:String, inPort:Int)
-  {
-     trace(inHost + ":" + inPort);
-     try
-     {
-        server = new Socket();
-        var host = new Host(inHost);
-        Sys.println("Waiting for connection on " + host + ":" + inPort);
-        server.bind(host,inPort);
-        server.listen(1);
-        port = inPort;
-
-        // Wait for connection....
-        dbg = server.accept();
-
-        trace("Got connection:" + ok());
-        if (ok())
+    function new(inHost:String, inPort:Int)
+    {
+        trace(inHost + ":" + inPort);
+        try
         {
-           fromApp = dbg.input;
-           toApp = dbg.output;
+            server = new Socket();
+            var host = new Host(inHost);
+            Sys.println("Waiting for connection on " + host + ":" + inPort);
+            server.bind(host,inPort);
+            server.listen(1);
+            port = inPort;
+
+            // Wait for connection....
+            dbg = server.accept();
+
+            trace("Got connection:" + ok());
+            if (ok())
+            {
+                fromApp = dbg.input;
+                toApp = dbg.output;
+            }
         }
-     }
-     catch(e:Dynamic)
-     {
-        Sys.println("Could not create dbg server on " + inHost + ":" + inPort);
-     }
+        catch(e:Dynamic)
+        {
+            Sys.println("Could not create dbg server on " + inHost + ":" + inPort);
+        }
 
-  }
+    }
 
-  public function ok() { return dbg!=null; }
+    public function ok() { return dbg!=null; }
 
-  function readString()
-  {
-     var len = fromApp.readInt32();
-     return fromApp.readString(len);
-  }
+    function readString()
+    {
+        var len = fromApp.readInt32();
+        return fromApp.readString(len);
+    }
 
 
-  function readLoop()
-  {
-     try
-     {
+    function readLoop()
+    {
+        try
+        {
+            while(going)
+            {
+                var code = fromApp.readByte();
+                if (code > DebugServer.output)
+                {
+                    Sys.println("Unknown message code : " + code);
+                    mainThread.sendMessage("bye");
+                    return;
+                }
+                var value = readString();
+                switch (code)
+                {
+                    case DebugServer.output:
+                        Sys.println(value);
+                    case DebugServer.status:
+                        Sys.println(value);
+                    case DebugServer.result:
+                        mainThread.sendMessage(value);
+                }
+            }
+        }
+        catch (e:Dynamic)
+        {
+            going = false;
+            Sys.println("Connection terminated:" + e);
+            mainThread.sendMessage("bye");
+        }
+        trace("readLoop done");
+    }
+
+
+    function mainLoop( )
+    {
+        trace("mainLoop");
+        going = true;
+
+        mainThread = Thread.current();
+        readThread = Thread.create(readLoop);
+        var stdin = Sys.stdin();
+
         while(going)
         {
-           var code = fromApp.readByte();
-           if (code > DebugServer.output)
-           {
-              Sys.println("Unknown message code : " + code);
-              mainThread.sendMessage("bye");
-              return;
-           }
-           var value = readString();
-		   switch (code)
-		   {
-			   case DebugServer.output:
-					Sys.println(value);
-				case DebugServer.status:
-					Sys.println(value);
-				case DebugServer.result:
-					mainThread.sendMessage(value);
-		   }
+            Sys.print(" => ");
+            var command = stdin.readLine();
+
+            if (command!="")
+            {
+                toApp.writeString( command + "\n" );
+                // Wait result...
+                var result = Thread.readMessage(true);
+                Sys.println(result);
+                if (result=="bye")
+                {
+                    going = false;
+                    try { fromApp.close(); } catch (e:Dynamic) { }
+                    try { toApp.close(); } catch (e:Dynamic) { }
+                }
+            }
         }
-     }
-     catch (e:Dynamic)
-     {
-        going = false;
-        Sys.println("Connection terminated:" + e);
-        mainThread.sendMessage("bye");
-     }
-     trace("readLoop done");
-  }
-
-
-  function mainLoop( )
-  {
-     trace("mainLoop");
-     going = true;
-
-     mainThread = Thread.current();
-     readThread = Thread.create(readLoop);
-     var stdin = Sys.stdin();
-
-     while(going)
-     {
-        Sys.print(" => ");
-        var command = stdin.readLine();
-
-        if (command!="")
-        {
-           toApp.writeString( command + "\n" );
-           // Wait result...
-           var result = Thread.readMessage(true);
-           Sys.println(result);
-           if (result=="bye")
-           {
-              going = false;
-              try { fromApp.close(); } catch (e:Dynamic) { }
-              try { toApp.close(); } catch (e:Dynamic) { }
-           }
-        }
-     }
-     trace("Bye bye");
-  }
+        trace("Bye bye");
+    }
 
 
 
-  public static function main()
-  {
-      var args = Sys.args();
-      /*
-      for(arg in args)
-         if (arg=="-break")
-            pause = true;
-      */
-      var server = new DebugServer(Host.localhost(),6495);
+    public static function main()
+    {
+        var args = Sys.args();
+        /*
+           for(arg in args)
+           if (arg=="-break")
+           pause = true;
+         */
+        var server = new DebugServer(Host.localhost(),6495);
 
-      if (server.ok())
-         server.mainLoop();
-  }
+        if (server.ok())
+            server.mainLoop();
+    }
 }
